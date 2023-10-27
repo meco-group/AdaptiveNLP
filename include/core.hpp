1ab82487baa5ccb276dcc58c14c94f074086e9bc
callenslouis <louis.callens@kuleuven.be>
2023-10-27 10:10:57 +0200

    update to repo structure

---
+++ include/core.hpp
@@ -0,0 +1,11 @@
+#ifndef __CORE_HPP_
+#define __CORE_HPP_
+
+#include "adaptiveNLP.hpp"
+#include "bookkeeper.hpp"
+#include "buildingBlocks.hpp"
+#include "customExceptions.hpp"
+#include "interfaceTester.hpp"
+#include "NLPInterface.hpp"
+
+#endif
